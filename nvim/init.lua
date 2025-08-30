8c4e69553e260b70e589d89d0585a9e5b564604a
A Rohit <95871915+togetherlab@users.noreply.github.com>
2025-08-30 15:15:45 +0530

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require("rohit")
+
+-- hello fem
---
+++ nvim/lua/rohit/lazy/cellular-automaton.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "eandrju/cellular-automaton.nvim",
+        config = function()
+            -- The plugin is configured in remap.lua with the <leader>ca keybinding
+            -- This ensures it gets loaded properly
+        end,
+    },
+}
